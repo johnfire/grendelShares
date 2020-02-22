b16d3f8ced307aa6df0c640a19560a9e8aaf0bba
chris Rehm <christopherrehm@web.de>
2020-02-23 00:57:23 +0100

    async message passing no transmission losses

--- grendelconfig.py
+++ grendelconfig.py
@@ -1,11 +1,51 @@
 
+import os
+import json
+
+
 msgPath = "/media/grendelData102/GrendelData/grendelMsgs/"
 msgPathAI = "/media/grendelData102/GrendelData/grendelMsgs/AI"
 msgPathPY = "/media/grendelData102/GrendelData/grendelMsgs/PY"
-msgPathOT = "/media/grendelData102/GrendelData/grendelMsgs/OTW"
+msgPathOT = "/media/grendelData102/GrendelData/grendelMsgs/OT"
 fotoPath = "/media/grendelData102/GrendelData/grendelFotos/"
 audioPath = "/media/grendelData102/GrendelData/grendellAudio/"
 grendelOtherData = "/media/grendelData102/GrendelData/grendelOtherData/"
 grendelPeopleData = "/media/grendelData102/GrendelData/grendelPeopleData/"
 processFotoPath ="/media/grendelData102/lowLevelPrograms/processFoto.py "
+
+class message():
+
+    def write(timeStamp, title, text, primeRecipient, priority, sender, otherRecipients, files ):
+        print(timeStamp)
+
+        myCurrentDir = os.getcwd()
+        mydata = [timeStamp, title, text, primeRecipient, priority, sender, otherRecipients, files]
+        jsonData = json.dumps(mydata, sort_keys = True,  indent = 4, separators = (",", ": "))
+        filename = str(timeStamp)
+        if primeRecipient == "AI":
+            os.chdir(msgPathAI)
+        elif primeRecipient == "PY":
+            os.chdir(msgPathPY)
+        elif primeRecipient == "OT":
+            os.chdir(msgPathOT)
+        with open(filename, 'w') as f:
+             f.write(jsonData)
+        os.chdir(myCurrentDir)
+
+
+    def read(timeStamp, program):
+        myCurrentDir = os.getcwd()
+        filename = timeStamp
+        if program == "AI":
+            os.chdir(msgPathAI)
+        elif program == "PY":
+            os.chdir(msgPathPY)
+        elif program == "OT":
+            os.chdir(msgPathOT)
+        with open(filename, "r") as content:
+            datastuff = json.load(content)
+            return datastuff
+        os.chdir(myCurrentDir)
+
+
 
