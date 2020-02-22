c2e61aa5bf0a47f00cb580c4201ae55aed77f5da
chris Rehm <christopherrehm@web.de>
2020-02-22 19:28:08 +0100

    upload this file to this repository

---
+++ grendelconfig.py
@@ -0,0 +1,11 @@
+
+msgPath = "/media/grendelData102/GrendelData/grendelMsgs/"
+msgPathAI = "/media/grendelData102/GrendelData/grendelMsgs/AI"
+msgPathPY = "/media/grendelData102/GrendelData/grendelMsgs/PY"
+msgPathOT = "/media/grendelData102/GrendelData/grendelMsgs/OTW"
+fotoPath = "/media/grendelData102/GrendelData/grendelFotos/"
+audioPath = "/media/grendelData102/GrendelData/grendellAudio/"
+grendelOtherData = "/media/grendelData102/GrendelData/grendelOtherData/"
+grendelPeopleData = "/media/grendelData102/GrendelData/grendelPeopleData/"
+processFotoPath ="/media/grendelData102/lowLevelPrograms/processFoto.py "
+
